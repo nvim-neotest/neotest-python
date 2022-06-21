59e6d0d8b8c1004881a15bcc0b25a7f5c40e5cee
Nick Yasnogorodskyi <nickyasnogorodskyi@Nicks-MacBook-Pro.local>
2022-06-21 11:59:40 +0300

    feat: make python path probing async

--- lua/neotest-python/base.lua
+++ lua/neotest-python/base.lua
@@ -21,50 +21,50 @@ M.module_exists = function(module, python_command)
   })) == 0
 end
 
-local mem = {}
+local python_command_mem = {}
 
 ---@return string[]
 function M.get_python_command(root)
-  if mem[root] then
-    return mem[root]
+  if python_command_mem[root] then
+    return python_command_mem[root]
   end
   -- Use activated virtualenv.
   if vim.env.VIRTUAL_ENV then
-    mem[root] = { Path:new(vim.env.VIRTUAL_ENV, "bin", "python").filename }
-    return mem[root]
+    python_command_mem[root] = { Path:new(vim.env.VIRTUAL_ENV, "bin", "python").filename }
+    return python_command_mem[root]
   end
 
   for _, pattern in ipairs({ "*", ".*" }) do
     local match = async.fn.glob(Path:new(root or async.fn.getcwd(), pattern, "pyvenv.cfg").filename)
     if match ~= "" then
-      mem[root] = { (Path:new(match):parent() / "bin" / "python").filename }
-      return mem[root]
+      python_command_mem[root] = { (Path:new(match):parent() / "bin" / "python").filename }
+      return python_command_mem[root]
     end
   end
 
   if lib.files.exists("Pipfile") then
-    local f = assert(io.popen("pipenv --py 2>/dev/null", "r"))
-    local venv = assert(f:read("*a")):gsub("\n", "")
-    f:close()
+    local _, data = lib.process.run({ "pipenv", "--py" }, { stdout = true })
+    local venv = data.stdout:gsub("\n", "")
     if venv then
-      mem[root] = { Path:new(venv).filename }
-      return mem[root]
+      python_command_mem[root] = { Path:new(venv).filename }
+      return python_command_mem[root]
     end
   end
 
   if lib.files.exists("pyproject.toml") then
-    local f = assert(io.popen("poetry env info -p 2>/dev/null", "r"))
-    local venv = assert(f:read("*a")):gsub("\n", "")
-    f:close()
+    local _, data = lib.process.run({ "poetry", "env", "info", "-p" }, { stdout = true })
+    local venv = data.stdout:gsub("\n", "")
     if venv then
-      mem[root] = { Path:new(venv, "bin", "python").filename }
-      return mem[root]
+      python_command_mem[root] = { Path:new(venv, "bin", "python").filename }
+      return python_command_mem[root]
     end
   end
 
   -- Fallback to system Python.
-  mem[root] = { async.fn.exepath("python3") or async.fn.exepath("python") or "python" }
-  return mem[root]
+  python_command_mem[root] = {
+    async.fn.exepath("python3") or async.fn.exepath("python") or "python",
+  }
+  return python_command_mem[root]
 end
 
 return M
